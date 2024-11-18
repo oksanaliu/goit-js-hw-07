3997a7a9f4e7ccd7577d9afc406a0be6cf95316d
oksanaliu <lyuklyan4@icloud.com>
2024-11-18 11:53:08 +0100

    changed

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 const categories = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${categories.length}`);
+
 categories.forEach((category) => {
   const title = category.querySelector('h2').textContent;
   const items = category.querySelectorAll('ul li').length;
