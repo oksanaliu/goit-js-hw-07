fcfc687758f3b3df03100b3c2550a0081a5f3edc
oksanaliu <lyuklyan4@icloud.com>
2024-11-18 12:31:35 +0100

    змінено

--- js/task-6.js
+++ js/task-6.js
@@ -4,17 +4,11 @@ const input = controls.querySelector('input[type="number"]');
 const createBtn = controls.querySelector('[data-create]');
 const destroyBtn = controls.querySelector('[data-destroy]');
 
-createBtn.addEventListener('click', () => {
-  const amount = parseInt(input.value, 10);
-  if (amount >= 1 && amount <= 100) {
-    createBoxes(amount);
-    input.value = '';
-  } else {
-    alert('Please enter a number between 1 and 100');
-  }
-});
-
-destroyBtn.addEventListener('click', destroyBoxes);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
 function createBoxes(amount) {
   destroyBoxes();
@@ -37,8 +31,14 @@ function destroyBoxes() {
   boxesContainer.innerHTML = '';
 }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, '0')}`;
-}
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value, 10);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
